c3cdf783dd8f13dfd4dca1773d0bb7fc63728df1
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2022-05-22 22:40:16 -0300

    Update User.php

--- controle_veiculos/app/User.php
+++ controle_veiculos/app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function proprietario_id(){
+        return hasMany(proprietario_id::class,'proprietario_id');
+    }
 }
